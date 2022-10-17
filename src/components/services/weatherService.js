7c2d0ed5be8e634ef5b9a7e0248b5a9420ccb656
mehboob-ali <mehboobalishaikh50@gmail.com>
2022-10-17 11:22:03 +0530

    date and time can be converted to local time using Luxon liabrary

--- src/components/services/weatherService.js
+++ src/components/services/weatherService.js
@@ -57,6 +57,11 @@ export const getFormattedWeatherData = async (searchParams)=>{
    
     return formattedCurrentWeather;
 }
+const formatToLocalTime=( 
+    secs, 
+    zone,
+    format= "cccc, dd LLL yyyy' | Local time: 'hh:mm a")=> DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
 
 
 export default getFormattedWeatherData;
